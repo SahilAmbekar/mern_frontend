10b1cc41e7114b6296584649fd290b107dcd2b2a
Sahil Ambekar <sahil.ambekar@globant.com>
2024-07-14 12:51:45 +0530

    ADD: Ratings component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,19 +2,28 @@
 import * as React from 'react';
 import {Container, Nav, Navbar} from "react-bootstrap";
 import {FaHome, FaShoppingCart, FaUser} from "react-icons/fa";
+import {LinkContainer} from "react-router-bootstrap";
 
 export function Header() {
     return (
         <header>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                <LinkContainer to={"/"}>
+                    <Navbar.Brand>ProShop</Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="/cart"><FaShoppingCart /> Cart</Nav.Link>
-                        <Nav.Link href="/login"><FaUser /> Sign In</Nav.Link>
-                        <Nav.Link href="/home"><FaHome/>Home</Nav.Link>
+                        <LinkContainer to={"/cart"}>
+                            <Nav.Link><FaShoppingCart /> Cart</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to={"/login"}>
+                            <Nav.Link><FaUser /> Sign In</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to={"/home"}>
+                            <Nav.Link><FaHome/>Home</Nav.Link>
+                        </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
